bfaed571a0bda7ea2e468ed161793da75dbf81f9
MikolajP13 <mikolajp1313@gmail.com>
2023-04-03 17:32:32 +0200

    view binding update

--- RecyclerViewApp/app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ RecyclerViewApp/app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -6,19 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.LinearLayout
-import android.widget.LinearLayout.HORIZONTAL
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewapp.databinding.ItemMainAdapterBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var parentLayout: LinearLayout
+//    lateinit var parentLayout: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 //        parentLayout = findViewById(R.id.mainActivityList)
 
         val recyclerData = mutableListOf<RecyclerItemData>()
@@ -37,22 +37,32 @@ class MainActivity : AppCompatActivity() {
         val recycler = findViewById<RecyclerView>(R.id.mainActivityList)
         recycler.adapter = adapter
         recycler.layoutManager = LinearLayoutManager(this)
+
     }
 
-    class TestViewHolder (view: View) : RecyclerView.ViewHolder(view){
+    class TestViewHolder(binding: ItemMainAdapterBinding) : RecyclerView.ViewHolder(binding.root){
         val textView: TextView
         val button: Button
 
         init{
-            textView = view.findViewById(R.id.adapterItemTextView)
-            button = view.findViewById(R.id.adapterItemButton)
+            //textView = view.findViewById(R.id.adapterItemTextView)
+            //button = view.findViewById(R.id.adapterItemButton)
+            textView = binding.adapterItemTextView
+            button = binding.adapterItemButton
+
+            binding.adapterItemButton.setOnClickListener {
+                Toast.makeText(binding.root.context, binding.adapterItemTextView.text.toString()
+                    .uppercase(Locale.ROOT), Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
     class MainActivityAdapter(var data: List<RecyclerItemData>) : RecyclerView.Adapter<TestViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TestViewHolder {
-            val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_main_adapter, parent, false)
-            return TestViewHolder(view)
+            //val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_main_adapter, parent, false)
+            val binding = ItemMainAdapterBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+            return TestViewHolder(binding)
         }
 
         override fun onBindViewHolder(holder: TestViewHolder, position: Int) {
